5708bae35064cfb3ee1dfa8c1fd5ebd414bfa777
Ghazal J <ghazal.jain.97@gmail.com>
2021-11-01 22:50:04 -0700

    Stage Changes

--- Tetris/Assets/Scripts/GameMaster.cs
+++ Tetris/Assets/Scripts/GameMaster.cs
@@ -1,30 +1,57 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
 
 public class GameMaster : MonoBehaviour
 {
+    [Header("Tetrimino")]
     public GameObject[] TetriminoPrefabs;
     public static int height = 20;
     public static int width = 10;
     public float speed = 1.0f;
     public float coolOffTime;
+    [HideInInspector]
     public GameObject currentTetriminoFalling = null;
+    [HideInInspector]
     public GameObject nextTetriminoFalling = null;
     public static GameObject[,] grid = new GameObject[width, height];
+    
+    [Header("UI Data Management")]
     public static int rows = 0;
     public static int score = 0;
     public static int level = 1;
+    public int stage = 1;
+    public int rowsStage1 = 10;
+    public int rowsStage2 = 10;
+    public int rowsStage3 = 10;
+    private int rowsUpdate = 0;
+    private int stageRows = 0;
+    
+    [Header("UI Texts")]
     public Text hud_score;
     public Text hud_level;
     public Text hud_rows;
+    public Text hud_stageRows;
+    public Text hud_stage;
+    
+    [Header("Audio")]
+    
 
+    [HideInInspector]
     public GameObject NextTetrimino;
+    [HideInInspector]
     public GameObject PreviewTetrimino;
+    [HideInInspector]
     public bool gameStarted = false;
 
+    private void Awake()
+    {
+        GameInfoUpdate();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -39,8 +66,6 @@ public class GameMaster : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-
         if (coolOffTime < Time.time)
         {
             // instantiate a new tetrimino and move it down ...
@@ -102,7 +127,129 @@ public class GameMaster : MonoBehaviour
 
         return NextTetrimino;
     }
-    /*
+    
+
+    public bool CheckIsValidPosition()
+    {
+        foreach (GameObject cube in FindObjectOfType<Tetrimino>().cubes)
+        {
+            Vector2 v = Tetrimino.RoundVector(cube.transform.position);
+
+            if (!Tetrimino.IsInsideBorder(v))
+                return false;
+            if (grid[(int)v.x, (int)v.y] != null && grid[(int)v.x, (int)v.y].transform.parent.parent != currentTetriminoFalling.transform)
+                return false;
+        }
+
+        return true;
+    }
+
+    public bool CheckIsAboveGrid(GameObject tetrimino)
+    {
+        for(int x = 0; x < width; x++)
+        {
+            foreach(GameObject cube in FindObjectOfType<Tetrimino>().cubes)
+            {
+                Vector2 v = Tetrimino.RoundVector(cube.transform.position);
+                if ((int)v.y > height - 1)
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    public void GameInfoUpdate()
+    {
+        
+        //Debug.Log("Score = " + score);
+        hud_score.text = score.ToString();
+        level = (rows / 10)+ 1;
+        //Debug.Log("Level = " + level);
+        hud_level.text = level.ToString();
+
+        if (rows >= rowsStage1)
+        {
+            stage = 2;
+            
+        }
+        if (rows >= rowsStage1+rowsStage2)
+        {
+            stage = 3;
+        }
+        if (rows >= rowsStage1+rowsStage2+rowsStage3)
+        {
+           //You Win
+        }
+
+        if (stage == 1)
+        {
+            rowsUpdate = rows;
+            stageRows = rowsStage1;
+        }
+
+        if (stage == 2)
+        {
+            rowsUpdate = rows;
+            rowsUpdate -= rowsStage1;
+            stageRows = rowsStage2;
+        }
+
+        if (stage == 3)
+        {
+            rowsUpdate = rows;
+            rowsUpdate -= (rowsStage1+rowsStage2);
+            stageRows = rowsStage3;
+        }
+        hud_stage.text = stage.ToString();
+        //Debug.Log("Rows = " + rows);
+        hud_rows.text = rowsUpdate.ToString();
+        hud_stageRows.text = stageRows.ToString();
+    }
+
+    public void OutOfBounds(GameObject tetrimino)
+    {
+        for (int x = 0; x < width; ++x)
+        {
+            foreach (GameObject cube in FindObjectOfType<Tetrimino>().cubes)
+            {
+                Vector2 v = Tetrimino.RoundVector(cube.transform.position);
+                if (v.y > height - 1)
+                    GameOver();
+            }
+        }
+
+    }
+
+    public void Speed()
+    {
+        if (level == 1)
+            speed = 1.0f;
+        if (level == 2)
+            speed = 0.8f;
+        if (level == 3)
+            speed = 0.6f;
+        // if (level == 4)
+        //     speed = 0.5f;
+        // if (level == 5)
+        //     speed = 0.4f;
+        // if (level == 6)
+        //     speed = 0.3f;
+        // if (level == 7)
+        //     speed = 0.2f;
+        // if (level == 8)
+        //     speed = 0.1f;
+        // if (level > 8)
+        //     speed = 0.1f;
+    }
+
+    public void GameOver()
+    {
+        SceneManager.LoadScene("GameOver");
+
+    }
+}
+
+/*
     public GameObject SpawnNextTetrimino()
     {
 
@@ -182,86 +329,3 @@ public class GameMaster : MonoBehaviour
 
     }
      */
-
-    public bool CheckIsValidPosition()
-    {
-        foreach (GameObject cube in FindObjectOfType<Tetrimino>().cubes)
-        {
-            Vector2 v = Tetrimino.RoundVector(cube.transform.position);
-
-            if (!Tetrimino.IsInsideBorder(v))
-                return false;
-            if (grid[(int)v.x, (int)v.y] != null && grid[(int)v.x, (int)v.y].transform.parent.parent != currentTetriminoFalling.transform)
-                return false;
-        }
-
-        return true;
-    }
-
-    public bool CheckIsAboveGrid(GameObject tetrimino)
-    {
-        for(int x = 0; x < width; x++)
-        {
-            foreach(GameObject cube in FindObjectOfType<Tetrimino>().cubes)
-            {
-                Vector2 v = Tetrimino.RoundVector(cube.transform.position);
-                if ((int)v.y > height - 1)
-                    return true;
-            }
-        }
-        return false;
-    }
-
-    public void GameInfoUpdate()
-    {
-        Debug.Log("Rows = " + rows);
-        hud_rows.text = rows.ToString();
-        Debug.Log("Score = " + score);
-        hud_score.text = score.ToString();
-        level = (rows / 10)+ 1;
-        Debug.Log("Level = " + level);
-        hud_level.text = level.ToString();
-    }
-
-    public void OutOfBounds(GameObject tetrimino)
-    {
-        for (int x = 0; x < width; ++x)
-        {
-            foreach (GameObject cube in FindObjectOfType<Tetrimino>().cubes)
-            {
-                Vector2 v = Tetrimino.RoundVector(cube.transform.position);
-                if (v.y > height - 1)
-                    GameOver();
-            }
-        }
-
-    }
-
-    public void Speed()
-    {
-        if (level == 1)
-            speed = 1.0f;
-        if (level == 2)
-            speed = 0.8f;
-        if (level == 3)
-            speed = 0.6f;
-        // if (level == 4)
-        //     speed = 0.5f;
-        // if (level == 5)
-        //     speed = 0.4f;
-        // if (level == 6)
-        //     speed = 0.3f;
-        // if (level == 7)
-        //     speed = 0.2f;
-        // if (level == 8)
-        //     speed = 0.1f;
-        // if (level > 8)
-        //     speed = 0.1f;
-    }
-
-    public void GameOver()
-    {
-        SceneManager.LoadScene("GameOver");
-
-    }
-}
